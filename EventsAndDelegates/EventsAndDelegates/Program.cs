cb0939eb63c4d744f2b73d2cef00c59359d2f797
novvvi <novehui@gmail.com>
2020-05-17 16:20:07 -0500

    EventsAndDelegates

---
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video" };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+            var messageService = new MessageService();
+
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+
+        // 1- define a delegate
+        // 2- define an event based on that delegate
+        // 3- Raise the event
+
+        // public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        // public event VideoEncodedEventHandler VideoEncoded;
+
+
+        // ing form represent happen before the method call , and pass tense "ed" represent after the method called.
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() {Video = video});
+                // EventArgs.Empty without addition data
+            }
+        }
+    }
+}
